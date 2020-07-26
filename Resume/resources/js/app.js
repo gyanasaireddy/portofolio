fcead2da84db528b71859d3bfca6f631880f77da
gyanasaireddy <gyanismart7@gmail.com>
2020-07-26 20:27:55 +0530

    first commit

---
+++ Resume/resources/js/app.js
@@ -0,0 +1,77 @@
+var htmlp=document.getElementById("htmlp")
+var htmlprogress = new ProgressBar.Line(htmlp, {
+    strokeWidth: 4,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: '#003D74',
+    trailColor: '#eee',
+    trailWidth: 1,
+    svgStyle: {width: '100%', height: '100%'}
+  });
+  htmlprogress.animate(0.8); 
+
+var js=document.getElementById("js")
+
+var jsprogress = new ProgressBar.Line(js, {
+    strokeWidth: 4,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: '#003D74',
+    trailColor: '#eee',
+    trailWidth: 1,
+    svgStyle: {width: '100%', height: '100%'}
+  });
+  jsprogress.animate(0.85); 
+
+  var angular=document.getElementById("angular")
+  var angularprogress = new ProgressBar.Line(angular, {
+    strokeWidth: 4,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: '#003D74',
+    trailColor: '#eee',
+    trailWidth: 1,
+    svgStyle: {width: '100%', height: '100%'}
+  });
+  angularprogress.animate(0.4); 
+
+  var react=document.getElementById("react")
+  var angularprogress = new ProgressBar.Line(react, {
+    strokeWidth: 4,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: '#003D74',
+    trailColor: '#eee',
+    trailWidth: 1,
+    svgStyle: {width: '100%', height: '100%'}
+  });
+  angularprogress.animate(0.4); 
+
+  var c=document.getElementById("c")
+  var cprogress = new ProgressBar.Line(c, {
+    strokeWidth: 4,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: '#003D74',
+    trailColor: '#eee',
+    trailWidth: 1,
+    svgStyle: {width: '100%', height: '100%'}
+  });
+  cprogress.animate(0.75); 
+
+  var ds=document.getElementById("ds")
+  var dsprogress = new ProgressBar.Line(ds, {
+    strokeWidth: 4,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: '#003D74',
+    trailColor: '#eee',
+    trailWidth: 1,
+    svgStyle: {width: '100%', height: '100%'}
+  });
+  dsprogress.animate(0.9); 
+
+
+//   End of Progress bar
+
+// var resume=do
